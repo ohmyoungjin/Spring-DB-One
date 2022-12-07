c8eecba09819571fd786d68ec1bf114e113ad3a5
ohmyoungjin <sisaudwls@naver.com>
2022-12-07 15:56:31 +0900

    20221205 :: Spring-DB-One UnCheck Exception 활용

---
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -0,0 +1,72 @@
+package hello.jdbc.exception.basic;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class UnCheckedAppTest {
+
+    @Test
+    void checked() {
+        Controller controller = new Controller();
+        assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(Exception.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request(){
+            service.logic();
+        }
+    }
+
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic(){
+            repository.call();
+            networkClient.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call()  {
+            throw new RuntimeConnectException("ex");
+        }
+    }
+
+    //리포지토리에서 체크 예외인 SQLException 이 발생하면 런타임 예외인 RuntimeSQLException 으로
+    //전환해서 예외를 던진다.
+    //참고로 이때 기존 예외를 포함해주어야 예외 출력시 스택 트레이스에서 기존 예외도 함께 확인할 수 있다.
+    static class Repository {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        public void runSQL() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+    static class RuntimeConnectException extends RuntimeException {
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
