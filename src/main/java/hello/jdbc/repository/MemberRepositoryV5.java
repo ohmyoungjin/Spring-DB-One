dee47c607b99e67085b21b8ad5d74a4f7537c30d
ohmyoungjin <sisaudwls@naver.com>
2022-12-08 09:46:31 +0900

    20221208 :: Spring-DB-One Spring JdbcTemplate 적용

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV5.java
@@ -0,0 +1,74 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import javax.sql.DataSource;
+
+
+/**
+ * JdbcTemplate 사용
+ */
+@Slf4j
+public class MemberRepositoryV5 implements MemberRepository {
+
+    private final JdbcTemplate template;
+
+    public MemberRepositoryV5(DataSource dataSource) {
+        this.template = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+        // 이 한줄로 밑에 있는 connection 맺고, PreparedStatement update 해주는 기능을 한다.
+        template.update(sql, member.getMemberId(), member.getMoney());
+        return member;
+        /*Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            throw exTranslator.translate("save", sql, e);
+        }finally {
+            close(con, pstmt, null);
+        }*/
+    }
+
+    @Override
+    public Member findById(String memberId) {
+        String sql = "select * from member where member_id =?";
+        Member member = template.queryForObject(sql, memberRowMapper(), memberId);
+        return member;
+    }
+
+    @Override
+    public void update(String memberId, int money) {
+        String sql = "update member set money=? where member_id=?";
+        template.update(sql, money, memberId);
+    }
+
+    @Override
+    public void delete(String memberId) {
+        String sql = "delete from member where member_id=?";
+
+        template.update(sql, memberId);
+    }
+
+
+    private RowMapper<Member> memberRowMapper() {
+        return (rs, rowNum) -> {
+            Member member = new Member();
+            member.setMemberId(rs.getString("member_id"));
+            member.setMoney(rs.getInt("money"));
+            return member;
+        };
+    }
+
+}
